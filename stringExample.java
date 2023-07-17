24a2414ed0b1dd7574c150d291a9b4dc38088d6a
vpccode <138013862+vpccode@users.noreply.github.com>
2023-07-17 23:32:56 +0530

    First Commit

---
+++ stringExample.java
@@ -0,0 +1,21 @@
+import java.lang.String;
+
+// for practies
+public class stringExample{
+
+public static void main(String args[]){
+
+String s1="Pooja sen verma";
+
+String s2=new String("Saira");
+
+char ch[]={'p','o','o','j','a'};
+
+String s3=new String(ch);
+
+System.out.println(s3);
+System.out.println(s1);
+System.out.println(s2);
+}
+}
+
